3cebc28e347a370c223335c20b7f9761406dd039
Anthony Foster <git@af.id.au>
2015-06-18 00:26:22 +1000

    Add comments.

--- APIClient/APIClient.h
+++ APIClient/APIClient.h
@@ -10,6 +10,7 @@
 
 typedef void (^Callback)(NSError *error, id response);
 
+//! Error domain for APIClient
 extern __attribute__((visibility ("default"))) NSString *const APIErrorDomain;
 
 typedef NS_ENUM(NSInteger, APIClientErrorCode) {
@@ -20,15 +21,51 @@ typedef NS_ENUM(NSInteger, APIClientErrorCode) {
     APIClientCannotConnectErrorCode = kCFURLErrorCannotConnectToHost
 };
 
+/*!
+ @class APIClient
+ 
+ @abstract
+ Generic API Client
+ 
+ @discussion
+ The Authorization header is set to 'Bearer {{accessToken}}' if it is not nil.
+ All requests are relative to baseUrl.
+*/
 @interface APIClient : NSObject
-@property (nonatomic) NSString *accessToken;
-@property (nonatomic) NSURL    *baseURL;
 
+
+/*! 
+ @method accessToken:
+ @abstract Sets the token used in the HTTP request 'Authorization' header.
+ @param method the new access token.
+ */
+@property (nonatomic) NSString *accessToken;
+
+
+/*! 
+ @method baseURL:
+ @abstract Sets the URL that requests are relative to. This value is required.
+ @param base URL for all HTTP requests
+ */
+@property (nonatomic) NSURL *baseURL;
+
+
+/* Send a GET request to a path. */
 - (void) GET    : (NSString *) path                              done:(Callback) done;
+
+/* Send a GET request to a path with query string parameters. */
 - (void) GET    : (NSString *) path query:(NSDictionary *)query  done:(Callback) done;
+
+/* Send JSON data in a PUT request. */
 - (void) PUT    : (NSString *) path  body:(NSDictionary *)body   done:(Callback) done;
+
+/* Send JSON data in a POST request. */
 - (void) POST   : (NSString *) path  body:(NSDictionary *)body   done:(Callback) done;
+
+/* Send JSON data in a PATCH request. */
 - (void) PATCH  : (NSString *) path  body:(NSDictionary *)body   done:(Callback) done;
+
+/* Send a DELETE request to a path. */
 - (void) DELETE : (NSString *) path                              done:(Callback) done;
 
 @end
